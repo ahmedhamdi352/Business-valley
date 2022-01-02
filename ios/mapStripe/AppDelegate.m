df2a46f9712cce9c31d8f9c3f9a1758f08533b90
ahmedhamdi <ahmedhamdi352@gmail.com>
2022-01-02 14:09:24 +0200

    remove key of google service

--- ios/mapStripe/AppDelegate.m
+++ ios/mapStripe/AppDelegate.m
@@ -27,7 +27,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-      [GMSServices provideAPIKey:@"AIzaSyCczLOZ-1vyQfWl5ti_Nc_wOHuOP8S_c_0"];
+      [GMSServices provideAPIKey:@""];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
